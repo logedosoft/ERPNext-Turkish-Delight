520f0924aa4da97ec017d30ae6eb5ab9ef175b92
turkert@hotmail.com <turkert@hotmail.com>
2020-07-27 23:04:23 +0300

    Cari Hesap Ekstre Ozet raporu hazirlandi.

---
+++ erpnextturkish/erpnext_turkish/report/cari_ekstre_ozet/cari_ekstre_ozet.js
@@ -0,0 +1,75 @@
+// Copyright (c) 2016, Logedosoft Business Solutions and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Cari Ekstre Ozet"] = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"party_type",
+			"label": __("Party Type"),
+			"fieldtype": "Link",
+			"options": "Party Type",
+			"default": "Customer",
+			on_change: function() {
+				frappe.query_report.set_filter_value('party', "");
+			}
+		},
+		{
+			"fieldname":"party",
+			"label": __("Party"),
+			"fieldtype": "MultiSelectList",
+			"reqd": 1,
+			get_data: function(txt) {
+				if (!frappe.query_report.filters) return;
+
+				let party_type = frappe.query_report.get_filter_value('party_type');
+				if (!party_type) return;
+
+				return frappe.db.get_link_options(party_type, txt);
+			},
+			on_change: function() {
+				var party_type = frappe.query_report.get_filter_value('party_type');
+				var parties = frappe.query_report.get_filter_value('party');
+
+				if(!party_type || parties.length === 0 || parties.length > 1) {
+					frappe.query_report.set_filter_value('party_name', "");
+					frappe.query_report.set_filter_value('tax_id', "");
+					return;
+				} else {
+					var party = parties[0];
+					/*var fieldname = erpnext.utils.get_party_name(party_type) || "name";
+					frappe.db.get_value(party_type, party, fieldname, function(value) {
+						frappe.query_report.set_filter_value('party_name', value[fieldname]);
+					});*/
+
+					if (party_type === "Customer" || party_type === "Supplier") {
+						frappe.db.get_value(party_type, party, "tax_id", function(value) {
+							frappe.query_report.set_filter_value('tax_id', value["tax_id"]);
+						});
+					}
+				}
+			}
+		},
+		{
+			"fieldname":"tax_id",
+			"label": __("Tax Id"),
+			"fieldtype": "Data",
+			"hidden": 0
+		},
+		{
+			"fieldname": "presentation_currency",
+			"label": __("Currency"),
+			"fieldtype": "Select",
+			"default": "TRY",
+			"options": erpnext.get_presentation_currency_list()
+		}
+	]
+};
---
+++ erpnextturkish/erpnext_turkish/report/cari_ekstre_ozet/cari_ekstre_ozet.py
@@ -0,0 +1,106 @@
+# Copyright (c) 2013, Logedosoft Business Solutions and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe, erpnext
+from erpnext import get_company_currency, get_default_company
+from erpnext.accounts.report.utils import get_currency, convert_to_presentation_currency
+from frappe.utils import getdate, cstr, flt, fmt_money
+from frappe import _, _dict
+
+def execute(filters=None):
+	if not filters:
+		return [], []
+
+	columns, data = [], []
+
+	if validate_filters(filters):
+
+		columns = get_columns(filters)
+
+		data = frappe.db.sql(
+			"""
+			SELECT 
+				BELGE_TARIHI, BELGE_SAATI, TIP, ACIKLAMA, BORC, ALACAK
+			FROM 
+				LD_CARI_EKSTRE_GENEL
+			WHERE 
+				SIRKET = %(company)s
+				AND CARI_KODU = %(party)s
+
+			UNION ALL
+
+			SELECT
+				'' AS BELGE_TARIHI,
+				'' AS BELGE_SAATI,
+				'' AS TIP,
+				'' AS ACIKLAMA,
+				SUM(BORC) AS BORC, SUM(ALACAK) AS ALACAK
+			FROM 
+				LD_CARI_EKSTRE_GENEL
+			WHERE 
+				SIRKET = %(company)s
+				AND CARI_KODU = %(party)s
+			"""
+			, filters, as_dict=1)
+
+	return columns, data
+
+def validate_filters(filters):
+	blnResult = True
+
+	if not filters.get('company'):
+		frappe.throw(_('{0} is mandatory').format(_('Company')))
+
+	if not filters.get('party'):
+		blnResult = False
+
+	return blnResult
+		
+def get_columns(filters):
+	if filters.get("company"):
+		currency = get_company_currency(filters["company"])
+	else:
+		company = get_default_company()
+		currency = get_company_currency(company)
+
+	columns = [
+		{
+			"label": _("Posting Date"),
+			"fieldname": "BELGE_TARIHI",
+			"fieldtype": "date",
+			"width": 90
+		},
+		{
+			"label": _("Posting Time"),
+			"fieldname": "BELGE_SAATI",
+			"fieldtype": "Time",
+			"width": 90
+		},
+		{
+			"label": _("Voucher Type"),
+			"fieldname": "TIP",
+			"fieldtype": "Data",
+			"width": 100
+		},
+		{
+			"label": _("Remark"),
+			"fieldname": "ACIKLAMA",
+			"fieldtype": "Data",
+			"width": 400
+		},
+		{
+			"label": _("Debit"),
+			"fieldname": "BORC",
+			"fieldtype": "Float",
+			"width": 100
+		},
+		{
+			"label": _("Credit"),
+			"fieldname": "ALACAK",
+			"fieldtype": "Float",
+			"width": 100
+		}
+	]
+
+	return columns
