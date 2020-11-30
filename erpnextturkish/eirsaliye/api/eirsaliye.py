747cd6be2ead36e3f2c06a7be4a63384eda09615
Youssef Restom <youssefrestom@gmail.com>
2020-11-30 14:49:44 +0000

    update: send_eirsaliye

--- erpnextturkish/eirsaliye/api/eirsaliye.py
+++ erpnextturkish/eirsaliye/api/eirsaliye.py
@@ -16,18 +16,33 @@ def send_eirsaliye(delivery_note_name):
     doc.db_update()
     frappe.db.commit()
     doc.reload()
+
     eirsaliye_settings = frappe.get_all("E Irsaliye Ayarlar", filters = {"company": doc.company})[0]
     settings_doc = frappe.get_doc("E Irsaliye Ayarlar", eirsaliye_settings)
-    # company_doc = frappe.get_doc("Company", doc.company)
-    set_warehouse_address_doc = frappe.get_doc("Address", get_default_address("Warehouse", doc.set_warehouse))
     
+    set_warehouse_address_doc = frappe.get_doc("Address", get_default_address("Warehouse", doc.set_warehouse))
+    set_warehouse_address_doc = set_missing_address_values(set_warehouse_address_doc)
+
     customer_doc = frappe.get_doc("Customer", doc.customer)
-    customer_address_doc = frappe.get_doc("Address", get_default_address("Customer", doc.customer))
+    customer_address_doc = frappe.get_doc("Address", doc.shipping_address_name)
+    customer_address_doc = set_missing_address_values(customer_address_doc)
     
     doc.eirsaliye_uuid = uuid.uuid1()
+
+    doc = set_driver_name(doc)
     
     user = {}
     user["full_name"] = frappe.get_value("User",frappe.session.user,"full_name")
+
+    for item in doc.items:
+        td_eirsaliye_birimi = frappe.get_list("TD EIrsaliye Birim Eslestirme",
+            filters={
+                "parent": settings_doc.name,
+                "td_birim": item.uom,    
+            },
+            fields = ["td_eirsaliye_birimi"]
+        )[0]["td_eirsaliye_birimi"]
+        item.td_eirsaliye_birimi = td_eirsaliye_birimi
 
 
     data_context = {
@@ -58,6 +73,7 @@ def send_eirsaliye(delivery_note_name):
     }
     body = render_template("eirsaliye_body.xml", body_context)
     body = body.encode('utf-8')
+    frappe.msgprint(str(body))
     session = requests.session()
     session.headers = {"Content-Type": "text/xml; charset=utf-8"}
     session.headers.update({"Content-Length": str(len(body))})
@@ -85,3 +101,41 @@ def send_eirsaliye(delivery_note_name):
 
 
 
+def set_missing_address_values(address_doc):
+    city_subdivision_name = ""
+    city_name = ""
+    if address_doc.city:
+        if "/" in address_doc.city:
+            city_split = address_doc.city.split('/')
+            city_subdivision_name = city_split[1]
+            city_name = city_split[0]
+        else:
+            city_name = address_doc.city
+    
+    address_doc.city_subdivision_name = city_subdivision_name
+    address_doc.city_name = city_name
+
+    return address_doc
+
+
+def set_driver_name(doc):
+    driver_first_name = ""
+    driver_family_name = ""
+    if doc.driver_name:
+        driver_name_split = doc.driver_name.split(' ')
+        if len(driver_name_split) == 1:
+            driver_first_name = doc.driver_name
+        elif len(driver_name_split) == 2:
+            driver_first_name = driver_name_split[0]
+            driver_family_name = driver_name_split[1]
+        elif len(driver_name_split) == 3:
+            driver_first_name = "{0} {1}".format(driver_name_split[0], driver_name_split[1]) 
+            driver_family_name = driver_name_split[2]
+        else:
+            driver_first_name = "{0} {1}".format(driver_name_split[0], driver_name_split[1]) 
+            driver_family_name = "{0} {1}".format(driver_name_split[2], driver_name_split[3])
+        
+    doc.driver_first_name = driver_first_name
+    doc.driver_family_name = driver_family_name
+
+    return doc
