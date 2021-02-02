00f911a93a152650bc98043bb959d2f0a7b03af8
Turker TUNALI <turkert@hotmail.com>
2021-02-02 08:02:30 +0000

    Version string updated

--- erpnextturkish/__init__.py
+++ erpnextturkish/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 import frappe
 
-__version__ = '1.210113.1'
+__version__ = '1.210201.1'
 
 def console(*data):
 	frappe.publish_realtime('out_to_console', data, user=frappe.session.user)
