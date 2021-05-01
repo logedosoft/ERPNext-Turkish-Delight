e5c2d1cdcb095213f38eed61530ec6c05387b260
Sinem Erdogan <ssnmerdgn@gmail.com>
2021-05-01 14:39:20 +0000

    feat:Sipariş Üretimde ambar transferleri yapıldı.

--- erpnextturkish/selling/api/selling_utils.py
+++ erpnextturkish/selling/api/selling_utils.py
@@ -18,27 +18,11 @@ def get_work_order_items(strSalesOrder):
 
     return docSO.get_work_order_items(for_raw_material_request = 1)#It won't check existing WOs
 
-# @frappe.whitelist()
-# def create_manufacture_se_for_so(items, company, sales_order, s_warehouse, t_warehouse):
-#     #Creates manufacture stock entries with given BOM and qty
-#     lstSE = []
-
-#     items = json.loads(items).get('items')
-
-#     for dctItem in items:
-#         dctSE = create_manufacture_se(dctItem['bom'], dctItem['required_qty'], company, s_warehouse, t_warehouse, sales_order)
-#         docSE = frappe.get_doc(dctSE)
-#         docSE.insert()
-#         docSE.submit()
-        
-#         lstSE.append(docSE.name)
-
-#     return lstSE
-
 @frappe.whitelist()
 def create_manufacture_se_for_so(items, company, sales_order, s_warehouse, t_warehouse):
     #Creates manufacture stock entries with given BOM and qty
-    lstSE = []
+    lstSE = [] #Olusturulan uretim fislerini tutar
+    lstSETransfer = [] #Olusturulacak ambar fisinde ki malzemeleri tutar
 
     items = json.loads(items).get('items')
 
@@ -55,27 +39,44 @@ def create_manufacture_se_for_so(items, company, sales_order, s_warehouse, t_war
             docSE.submit()
         
             lstSE.append(docSE.name)
-            
+        elif(docParent_Item_Group == "GRUP-Malzemeler"):
+            lstSETransfer.append({'item_code': docItem.item_code, 'uom': docItem.stock_uom, 'qty': dctItem['required_qty']})
+
+    #Ambtar transferi yapilacak malzemeler icin fis olusturalim
+    if len(lstSETransfer) > 0:   #lstSETransfer.count() 
+        print("AMBAR TR BASLATILIYOR")
+        dctSETransfer = create_transfer_se(lstSETransfer, company, sales_order, s_warehouse, t_warehouse)
+        docSETransfer = frappe.get_doc(dctSETransfer)
+        docSETransfer.insert()
+        docSETransfer.submit()
+        print("AMBAR AYIT TAMAM")
+
+        lstSE.append(docSETransfer.name)
+
     return lstSE
 
-def get_main_parent_item_group(strItemGroup):
-    #item group en başa kadar gidecek
-    blnControl= True
+def create_transfer_se(lstSETransfer, company, sales_order, s_warehouse, t_warehouse):
+    stock_entry = frappe.new_doc("Stock Entry")
+    stock_entry.purpose = "Material Transfer"
+    stock_entry.company = company
+    stock_entry.from_bom = 0
+    stock_entry.ld_sales_order = sales_order
 
-    lstItemGroup =[]
+    stock_entry.from_warehouse = s_warehouse
+    stock_entry.to_warehouse = t_warehouse
 
-    while (blnControl == True):
-        docItem_group = frappe.get_doc("Item Group", strItemGroup) 
-        if(docItem_group.parent_item_group != ""):
-            strItemGroup = docItem_group.parent_item_group
-            lstItemGroup.append(strItemGroup)
-        else:
-            blnControl =False
-    
-    if (blnControl ==False):
-        strItemGroup = lstItemGroup[(len(lstItemGroup)-2)]
+    stock_entry.set_stock_entry_type()
+    for item in lstSETransfer:
+        row = stock_entry.append('items', {})
+        print(item)
+        row.item_code = item['item_code']
+        row.qty = item['qty']
+        row.uom = item['uom']
 
-    return strItemGroup
+    stock_entry.set_actual_qty()
+    stock_entry.calculate_rate_and_amount(raise_error_if_no_rate=False)
+	
+    return stock_entry.as_dict()
 
 def create_manufacture_se(bom_no, qty, company, s_warehouse, t_warehouse, sales_order):
     stock_entry = frappe.new_doc("Stock Entry")
@@ -98,3 +99,22 @@ def create_manufacture_se(bom_no, qty, company, s_warehouse, t_warehouse, sales_
     stock_entry.calculate_rate_and_amount(raise_error_if_no_rate=False)
 	
     return stock_entry.as_dict()
+
+def get_main_parent_item_group(strItemGroup):
+    #item group en başa kadar gidecek
+    blnControl= True
+
+    lstItemGroup =[]
+
+    while (blnControl == True):
+        docItem_group = frappe.get_doc("Item Group", strItemGroup) 
+        if(docItem_group.parent_item_group != ""):
+            strItemGroup = docItem_group.parent_item_group
+            lstItemGroup.append(strItemGroup)
+        else:
+            blnControl =False
+    
+    if (blnControl ==False):
+        strItemGroup = lstItemGroup[(len(lstItemGroup)-2)]
+
+    return strItemGroup
