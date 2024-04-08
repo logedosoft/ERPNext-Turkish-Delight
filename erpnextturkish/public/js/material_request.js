5712c27ca6ffc100e48595e67de1f5513a7996e6
Turker TUNALI <turker.tunali@gmail.com>
2024-04-08 16:14:25 +0000

    Material Request, Variant Selector contd.

--- erpnextturkish/public/js/material_request.js
+++ erpnextturkish/public/js/material_request.js
@@ -4,12 +4,17 @@ frappe.ui.form.on("Material Request", {
 	onload: function(frm) {
 		//Check td utils, show_variant_selection_in_mr setting.https://app.asana.com/0/1199512727558833/1206652223240041/f
 		frappe.db.get_single_value("TD Utils", "show_variant_selection_in_mr").then( (r) => {
-			console.log(r);
 			if (r === 1) {
 				frm.toggle_display("custom_ld_variant_selector", true);
 			} else {
 				frm.toggle_display("custom_ld_variant_selector", false);
 			}
+		});
+	},
+	refresh: function (frm) {
+		//Show only items with variants
+		frm.set_query("item_template", "custom_ld_variant_selector", function (doc) {
+			return { "filters": { "has_variants": "1" } }
 		});
 	}
 });
