1b6c2278631ece95f278f55a666072cfc329dcb8
Turker Tunali <turker.tunali@gmail.com>
2025-12-12 13:04:02 +0300

    frappe import removed

--- erpnextturkish/__init__.py
+++ erpnextturkish/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
+#import frappe
 
 __version__ = '1.250711.7'
 
-def console(*data):
-	frappe.publish_realtime('out_to_console', data, user=frappe.session.user)
+#def console(*data):
+#	frappe.publish_realtime('out_to_console', data, user=frappe.session.user)
