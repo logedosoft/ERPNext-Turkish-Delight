3510d038264a4808d7850da1b5c9ba10732d18c2
Turker TUNALI <turker.tunali@gmail.com>
2026-01-23 10:34:40 +0000

    fix:init.py failed deploys

--- erpnextturkish/__init__.py
+++ erpnextturkish/__init__.py
@@ -1,8 +1,1 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 __version__ = '1.251210.2'
-
-def console(*data):
-	import frappe
-	frappe.publish_realtime('out_to_console', data, user=frappe.session.user)
